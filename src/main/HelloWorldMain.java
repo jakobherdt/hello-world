398865755ce8d3ebf5810e6e61956e45771bd229
Jakob Herdt <jakob.herdt@retest.de>
2017-12-22 09:10:59 +0100

    Add syso

--- src/main/HelloWorldMain.java
+++ src/main/HelloWorldMain.java
@@ -1,8 +1,22 @@
 package main;
 
-public class HelloWorldMain {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-	public static void main(String[] args) {
+public class HelloWorldMain {
+	
+	public Date parseDate(String dateTarget) throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'+'HH:mm");
+		Date parsedDate = dateFormat.parse(dateTarget);
+		return parsedDate;
+	}
+
+	public static void main(String[] args) throws ParseException {
+		HelloWorldMain hwm = new HelloWorldMain();
+		System.out.println(hwm.parseDate("2017-10-26T07:34:57.213228+00:00"));
+		
+		
 		String test = ": here are some changes";
 		System.out.println("Hello World "+test);
 	}
